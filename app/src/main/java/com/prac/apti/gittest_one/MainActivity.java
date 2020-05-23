038beb4154c69e0b832448e478d04a1c226c9e3b
ZsxPsBy <488266907@qq.com>
2020-05-23 23:26:05 +0800

    5.23

---
+++ app/src/main/java/com/prac/apti/gittest_one/MainActivity.java
@@ -0,0 +1,15 @@
+package com.prac.apti.gittest_one;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //撒打算看到
+      //  ..阿瑟东撒的阿瑟东
+    }
+}
