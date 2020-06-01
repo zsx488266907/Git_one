87105be1f7a976784dd637edf35393ea1c0847f9
ZsxPsBy <488266907@qq.com>
2020-06-01 22:56:24 +0800

    dsad

--- app/src/main/java/com/prac/apti/gittest_one/MainActivity.java
+++ app/src/main/java/com/prac/apti/gittest_one/MainActivity.java
@@ -3,10 +3,13 @@ package com.prac.apti.gittest_one;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 Button button;
+    private View viewById;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +17,11 @@ Button button;
         //撒打算看到
       //  ..阿瑟东撒的阿瑟东
 
+         //撒大苏打
+        Intent intent2= new Intent();
+        intent2.setClass(MainActivity.this,HomeActivity.class);
+
+        viewById = findViewById(R.id.button02);
         Intent intent = new Intent();
         intent.setClass(MainActivity.this,HomeActivity.class);
 
